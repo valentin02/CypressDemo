fae46cae3c467e6ce2b3aea565f097b9173bf049
Valentyn Pereverten <valentyn.pereverten@dataart.com>
2026-03-05 11:42:50 +0200

    changed test

--- cypress/e2e/OzanKaraliMedium/openOzanKarali.cy.js
+++ cypress/e2e/OzanKaraliMedium/openOzanKarali.cy.js
@@ -13,15 +13,14 @@ describe('Hello world project', () => {
     expect(1).to.equal(1) // цей тест завжди зелений
   })
 
-  // ❌ Тест, який спеціально падає
-  it('intentional fail test', () => {
-    cy.visit("https://medium.com/@someone/some-article", {
-      timeout: 0,
-      failOnStatusCode: false,
-    })
 
-    // Робимо гарантований фейл
-    expect(1).to.equal(2) 
-  })
+   it('go ozan karali medium - fail', () => {
+      cy.visit('https://medium.com/@someone/some-article', {
+         timeout: 120000,          // wait up to 120s for document.readyState=complete
+         failOnStatusCode: false
+      })
+      expect(1).to.equal(1)
+   });
+
 
 })
